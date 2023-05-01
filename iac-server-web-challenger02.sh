6b40fd07cd7d25de138b8a19e23d1ab47851a468
MarceloRavache <jm.ravache@outlook.com>
2023-05-01 19:56:07 -0300

    feat: create files of challenger aws cloud

---
+++ iac-server-web-challenger02.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Iniciando processo de criação do servidor web..."
+
+echo "Atualizando o servidor..."
+
+apt-get update
+apt-get upgrade -y
+
+echo "Instalando pacotes..."
+
+apt-get install apache2 -y
+apt-get install unzip -y
+
+echo "Baixando e copiando arquivos da aplicação..."
+
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html/
+
+echo "Finalizado processo..."
